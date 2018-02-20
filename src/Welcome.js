7412f66a644057a17d34338fe1125a2a2b650db9
palmerjoshua <palmerjoshua2013@fau.edu>
2018-02-19 19:51:48 -0500

    add welcome and license

---
+++ src/Welcome.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AboutMe extends Component {
+    render() {
+        return (
+            <div style={{marginLeft: 10}}>
+                <main>
+                    <h1>Welcome!</h1>
+                    <p>Take a look around. There's not much to see yet, but new features are coming soon!</p>
+                </main>
+                <footer><small>This page was overengineered using ReactJS | Copyright 2018 Joshua Palmer</small></footer>
+            </div>
+        );
+    }
+}
+
+export default AboutMe;
